e7052994cd37e02384968e1b95355ef185f34072
oknalv <thevlanko@gmail.com>
2020-02-04 03:06:14 +0100

    Work experience

--- actions.js
+++ actions.js
@@ -1,5 +1,99 @@
-const header = d3.select(".header-menu").select("header");
-let navigate = (path) => {
-    header.attr("class", null)
-    header.classed(path, true);
+import i18nKeyValues from "./i18n.js";
+const i18nElements = document.querySelectorAll('[data-i18n]');
+const langSelector = document.getElementById("lang-selector");
+const headerHome = document.getElementById("header-home");
+const sections = {
+    "home" : document.getElementById("home"),
+    "education" : document.getElementById("education"),
+    "experience" : document.getElementById("experience"),
+    "projects" : document.getElementById("projects")
+};
+const defaultLanguage = "en";
+const languageList = {
+    "en": "English",
+    "es": "español",
+    "fr": "français",
+    "gl": "galego"
 }
+const switchSectionTime = 500;
+const switchLanguageTime = 500;
+const switchTabTime = 250;
+
+let changingPath = false;
+
+document.body.setAttribute("style", `--switch-section-time: ${switchSectionTime}ms; --switch-language-time: ${switchLanguageTime}ms; --switch-tab-time: ${switchTabTime}ms`);
+
+window.navigate = (path) => {
+    let currentPath = document.body.classList[0];
+    if(!changingPath){
+        if(currentPath != path){
+            changingPath = true;
+            document.body.classList.remove("home", "education", "experience", "projects");
+            if(path == "home"){
+                headerHome.classList.add("home");
+                sections[path].classList.remove("hide");
+            }
+            else{
+                headerHome.classList.remove("home");
+            }
+            setTimeout(() => {
+                if(currentPath != "home"){
+                    sections[currentPath].classList.add("hide");
+                }
+                sections[path].classList.remove("hide");
+                setTimeout(() => {
+                    document.body.classList.add(path);
+                    changingPath = false;
+                }, switchSectionTime);
+            }, switchSectionTime);
+        }
+    }
+}
+
+const getTextForElement = (key, lang) => {
+    let text = i18nKeyValues[key][lang];
+    if(text == undefined){
+        console.warn(`'${key}' text key doesn't have a translation for '${lang}' language`);
+    }
+    return text == undefined ? i18nKeyValues[key][defaultLanguage] : text;
+}
+
+const changeLang = (lang, option) => {
+    if(!option.parentElement.classList.contains("selecting")){
+        option.parentElement.classList.add("selecting");
+    }
+    else {
+        for(let opt of langSelector.children){
+            opt.classList.remove("selected");
+        }
+        option.parentElement.classList.remove("selecting");
+        option.classList.add("selected");
+        for(let el of i18nElements){
+            el.innerHTML = getTextForElement(el.getAttribute("data-i18n"), lang);
+        }
+    }
+};
+
+window.closeLangSelector = () => {
+    langSelector.classList.remove("selecting");
+}
+
+const startLangSelector = () => {
+    for(let key in languageList){
+        let option = document.createElement("div");
+        langSelector.append(option);
+        option.id = `opt-${key}`;
+        option.onclick = () => changeLang(key, option);
+        option.textContent = languageList[key];
+    }
+    let browserLanguage = navigator.language.split("-")[0];
+    if(!(browserLanguage in languageList)){
+        browserLanguage = defaultLanguage;
+    }
+    document.getElementById(`opt-${browserLanguage}`).classList.add("selected");
+    for(let el of i18nElements){
+        el.innerHTML = getTextForElement(el.getAttribute("data-i18n"), browserLanguage);
+    }
+}
+
+startLangSelector();
---
+++ i18n.js
@@ -0,0 +1,118 @@
+const i18nKeyValues = {
+    "software-engineer": {
+        "en": "Software engineer",
+        "es": "Ingeniero de software",
+        "fr": "Ingénieur logiciel",
+        "gl": "Enxeñeiro de software"
+    },
+    "education": {
+        "en": "Education",
+        "es": "Educación",
+        "fr": "Formation",
+        "gl": "Educación"
+    },
+    "experience": {
+        "en": "Work experience",
+        "es": "Experiencia profesional",
+        "fr": "Expériences professionnelles",
+        "gl": "Experiencia profesional"
+    },
+    "projects": {
+        "en": "Personal projects",
+        "es": "Proyectos personales",
+        "fr": "Projets personnels",
+        "gl": "Proxectos persoais"
+    },
+    "home-text": {
+        "en": "Hello, my name is Eliot and I am a Spanish software engineer specialized in web development and the purpose of this page is to show you a little bit about me. Something to start with: I like programming, languages, comic books and cooking among other interesting things, but I suppose that you are not here to know this trivia about me and I am not here trying to convince you how cool my hobbies are, I suppose that you are here because you are considering to hire me so I invite you to check more relevant information about me in the different sections of this page by clicking on the titles down here."
+    },
+    "france": {
+        "en": "France",
+        "es": "Francia",
+        "fr": "France",
+        "gl": "Francia"
+    },
+    "spain": {
+        "en": "Spain",
+        "es": "España",
+        "fr": "Espagne",
+        "gl": "España"
+    },
+    "junior-programmer": {
+        "en": "Junior programmer",
+        "es": "Programador júnior",
+        "fr": "Programmeur junior",
+        "gl": "Programador júnior"
+    },
+    "software-engineer-rd": {
+        "en": "Software engineer at R&D department",
+        "es": "Ingeniero de software en el departamento de I+D",
+        "fr": "Ingénieur logiciel au département de R&D",
+        "gl": "Enxeñeiro de software no departamento de I+D"
+    },
+    "akka-1": {
+        "en": "Development of a web server from scratch, using Python, connected to a proof of concept of an autonomous car which retrieves data from its sensors. Its purpose is to serve this data to a later development of a web interface to display it in a human friendly way.",
+        "es": "Desarrollo de un servidor web desde cero, usando Python, conectado a una prueba de concepto de un coche autónomo que envía datos de sus sensores. Su propósito es servir esos datos a una implementación posterior de una interfaz web para mostrarlos de una manera más legible para el ser humano.",
+        "fr": "Développement d'un serveur web à partir de zéro, en utilisant Python, qui est connecté à une preuve de concept d'une voiture autonome qui envoie des données de ses capteurs. Le but est de servir ces données pour développer une interface web afin de les montrer d'une façon plus simple et conviviale pour les analyser.",
+        "gl": "Desenvolvemento dun servidor web desde cero, empregando Python, conectado a unha proba de concepto dun coche autónomo que envía datos dos seus sensores. O seu propósito é servir eses datos a unha implementación posterior dunha interface web para mostralos dunha maneira máis lexible para o ser humano."
+    },
+    "akka-2": {
+        "en": "Development of a web platform from scratch for the management of the projects' data of one of the departments of the company, developed using Angular and Angular Material in the front end, and Node.js, Express and TypeScript in the back end. The database is in MySQL. The methodology used for this project is Scrum in a distributed team.",
+        "es": "Desarollo de una plataforma web desde cero para la gestión de los datos de los proyectos de uno de los departamentos de la empresa, desarrollado usando Angular y Angular Material en el front end, y Node.js, Express y TypeScript en el back end. La base de datos utilizada es en MySQL. La metodología para este proyecto es Scrum en un equipo distribuido.",
+        "fr": "Développement d'une plate-forme web à partir de zéro pour gérer des données des projets d'un des départements de l'entreprise, développée en utilisant Angular et Angular Material pour le front end, et Node.js, Express et TypeScript pour le back end. La plate-forme utilise une base de données MySQL. La méthodologie utilisée pour ce project est Scrum dans une équipe de travail distribuée.",
+        "gl": "Desenvolvemento dunha plataforma web desde cero para a xestión dos datos dos proxectos dun dos departamentos da empresa, desenvolvido empregando Angular e Angular Material no front end, e Node.js, Express e TypeScript no back end. A base de datos empregada é en MySQL. A metodoloxía empregada para este proxecto é Scrum nun equipo distribuído."
+    },
+    "akka-3": {
+        "en": "Development of a software for the client Acore that communicates with a PLC which controls rectifiers. The software is developed in C# using WPF and Entity Framework with MySQL.",
+        "es": "Desarrollo de un programa para el cliente Acore que se comunica con un autómata programable que controla rectificadores. El programa está desarrollado en C# usando WPF y Entity Framework con MySQL.",
+        "fr": "Développement d'un logiciel pour le client Acore qui communique avec un automate programmable industriel qui contrôle des redresseurs. Le logiciel est développé en C# en utilisant les frameworks WPF et Entity Framework avec MySQL.",
+        "gl": "Desenvolvemento dun programa para o cliente Acore que se comunica cun autómata programable que controla rectificadores. O programa está desenvolvido en C# empregando WPF e Entity Framework con MySQL."
+    },
+    "akkademy": {
+        "en": "Before starting to work in France, I spent a moth (the month of August) in a training program called The Akkademy and conducted by Akka Switzerland in Geneva. The training was about improving soft skills with around 70 people from different parts of Europe and the working language was English. After that month, I was sent to Akka France in Lyon with several of my colleagues from Geneva."
+    },
+    "note": {
+        "en": "Note:",
+        "es": "Nota:",
+        "fr": "Note :",
+        "gl": "Nota:"
+    },
+    "imatia-1": {
+        "en": "Development and maintenance of a web based (SPA) logistics management application for Inditex. In the front end, this application uses mainly vanilla JavaScript with some jQuery (DOM manipulations, promise handling and communication with the back end REST API), Underscore (templates) and Backbone (routing), using also the IndexedDB API for storing temporary data. In the back end, this application uses Spring for the REST API endpoints as well as for the dependency injection manager, using complex SQL sentences in the database (DB2) layer for retrieving and adding data. This project was developed using the Scrum methodology mixed with some Kanban in a distributed team.",
+        "es": "Desarrollo y mantenimiento de una aplicación web (SPA) de gestión logística para Inditex. En el front end, esta aplicación utiliza JavaScript puro con algo de jQuery (manipulación del DOM, manejo de promesas y comunicación con la API REST del back end), Underscore (plantillas) y Backbone (routing), usando también la API de IndexedDB para almacenar datos temporales. En el back end, esta aplicación usa Spring para la API REST así como para el gestor de inyección de dependencias, usando sentencias SQL complejas en la capa de la base de datos (DB2) para consultar y añadir datos. Este proyecto está desarrollado con la metodología Scrum mezclada con un poco de Kanban en un equipo distribuido.",
+        "fr": "Développement et maintenance d'une application web (SPA) de gestion logistique pour Inditex. Dans le front end, cette application utilise JavaScript pur avec un peu de jQuery (manipulation du DOM, traitement des promesses et communication avec l'API REST du back end), Underscore (templates) et Backbone (routing), en utilisant aussi l'API IndexedDB pour enregistrer des données temporaires. Dans le back end, cette application utilise Spring pour faire les endpoints de l'API REST ainsi que pour l'injection de dépendances, en utilisant des instructions complexes SQL au niveau de la base de données (DB2). Ce projet est développé en utilisant la méthodologie Scrum combinée avec un peu de Kanban.",
+        "gl": "Desenvolvemento e mantemento dunha aplicación web (SPA) de xestión loxística para Inditex. No front end, esta aplicación emprega JavaScript puro con algo de jQuery (manipulación do DOM, manexo de promesas e comunicación coa API REST do back end), Underscore (templates) e Backbone (routing), empregando tamén a API de IndexedDB para almacenar datos temporais. No back end, esta apliación emprega Spring para a API rest así como para o xestor de inxección de dependencias, empregando sentencias SQL complexas na capa da base de datos (DB2) para consultar e engadir datos. Este proxecto está desenvolvido coa metodoloxía Scrum mesturada con un pouco de Kanban nun equipo distribuído."
+    },
+    "plexus-1": {
+        "en": "Software maintenance for Barceló Viajes.",
+        "es": "Mantenimiento de software para Barceló Viajes.",
+        "fr": "Maintenance des logiciels pour Barceló Viajes.",
+        "gl": "Mantemento de software para Barceló Viajes."
+    },
+    "redegal-1": {
+        "en": "Development of a customer clustering system for PrestaShop CMS using Spark MLlib.",
+        "es": "Desarrollo de un sistema de clustering de clientes para el CMS PrestaShop usando Spark MLlib.",
+        "fr": "Développement d'un système de clustering des données pour PrestaShop CMS en utilisant Spark MLlib.",
+        "gl": "Desenvolvemento dun sistema de clustering de clientes para o CMS PrestaShop empregando Spark MLlib."
+    },
+    "redegal-2": {
+        "en": "Development of the web interface of a route optimization system for garbage collection services, using AngularJS and LeafletJS in the front end, Node.js in the back end and an OSRM server.",
+        "es": "Desarrollo de una interfaz web de un sistema de optimización de rutas para servicios de recogida de basura, usando AngularJS y LeafletJS en el front end, Node.js en el back end y un servidor OSRM.",
+        "fr": "Développement d'une interface web pour un système d'optimisation des parcours pour les services de ramassage des ordures, en utilisant AnguarJS et LeafletJS dans le front end, Node.js dans le back end et un serveur OSRM.",
+        "gl": "Desenvolvemento dunha interface web dun sistema de optimización de rutas para servicios de recollida de basura, empregando AngularJS e LeafletJS no front end, Node.js no back end e un servidor OSRM."
+    },
+    "it-intern": {
+        "en": "IT intern",
+        "es": "Prácticas de administrador de sistemas",
+        "fr": "Étagiaire d'administrateur système",
+        "gl": "Prácticas de administrador de sistemas",
+    },
+    "concello-1": {
+        "en": "System administration in the Town Hall and the Police Department.",
+        "es": "Administrador de sistemas en el Ayuntamiento y la Policía Local.",
+        "fr": "Administrateur système dans la Mairie et le Département de Police.",
+        "gl": "Administrador de sistemas no Concello e na Policía Local."
+    }
+}
+
+export default i18nKeyValues;
