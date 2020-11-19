2649195f7fcf98a86c21cb1b0262340328f8fd31
oknalv <thevlanko@gmail.com>
2020-11-19 21:12:57 +0000

    New experience

--- i18n.js
+++ i18n.js
@@ -31,9 +31,9 @@ const i18nKeyValues = {
     },
     "home-text": {
         "en": "Hello, my name is Eliot and I am a Spanish software engineer specialized in web development and the purpose of this page is to show you a little bit about me. Something to start with: I love tortilla de patatas (without onion), churrasco, tartiflette, pierogi and salmorejo among other dishes, but, unless you want to invite me to dinner, let's focus on the reason why you are here, so I invite you to check more relevant information about me in the different sections of this page by clicking on the titles below.",
-        "es": "Hola, me llamo Eliot y soy un ingeniero de software español especializado en desarrollo web y el objetivo de esta página es mostraros un poco sobre mí. Algo con lo que empezar: me encanta la tortilla de patatas (sin cebolla), el churrasco, la tartiflette, los pierogi y el salmorejo entre otros platos, pero, a menos que queráis invitarme a cenar, centrémonos en la razón por la que estáis aquí, así que os invito a echarle un ojo a información sobre mí más relevante en las diferentes secciones de esta página haciendo click en los títulos de abajo.",
-        "fr": "Salut, je m'appelle Eliot et je suis un ingénieur logiciel spécialisé dans le développement web et le but de cette page est vous montrer un petit peu de moi. Pour commencer: j'adore la tortilla de patatas (sans oignon), le churrasco, la tartiflette, les pierogi et le salmorejo entre autres plats, mais, à moins que vous vouliez m'inviter à dîner, concentrons-nous sur la raison pour laquelle vous êtes ici, donc je vous invite à consulter des informations sur moi plus pertinentes dans les différentes sections de cette page en cliquant sur les titres ci-dessous.",
-        "gl": "Ola, chámome Eliot e sou un enxeñeiro de software español especializado en desenvolvemento web e o obxectivo desta páxina é amosarvos un pouco sobre min. Algo co que comezar: encántame a tortilla de patacas (sen cebola), o churrasco, a tartiflette, os pierogi e o salmorejo entre outros pratos, pero, a menos que queirades invitare a cear, centrémonos na razón pola que estades aquí, así que vos invito a botarlle un ollo a información sobre min máis relevante nas diferentes seccións desta páxina facendo click nos títulos de abaixo."
+        "es": "Hola, me llamo Eliot y soy un ingeniero de software español especializado en desarrollo web y el objetivo de esta página es mostraros un poco sobre mí. Algo con lo que empezar: me encanta la tortilla de patatas (sin cebolla), el churrasco, la tartiflette, los pierogi y el salmorejo entre otros platos, pero, a menos que queráis invitarme a cenar, centrémonos en la razón por la que estáis aquí, así que os invito a echarle un ojo a información más relevante sobre mí en las diferentes secciones de esta página haciendo click en los títulos de abajo.",
+        "fr": "Salut, je m'appelle Eliot et je suis un ingénieur logiciel spécialisé dans le développement web et le but de cette page est vous montrer un petit peu de moi. Pour commencer: j'adore la tortilla de patatas (sans oignon), le churrasco, la tartiflette, les pierogi et le salmorejo entre autres plats, mais, à moins que vous vouliez m'inviter à dîner, concentrons-nous sur la raison pour laquelle vous êtes ici, donc je vous invite à consulter des informations plus pertinentes sur moi dans les différentes sections de cette page en cliquant sur les titres ci-dessous.",
+        "gl": "Ola, chámome Eliot e sou un enxeñeiro de software español especializado en desenvolvemento web e o obxectivo desta páxina é amosarvos un pouco sobre min. Algo co que comezar: encántame a tortilla de patacas (sen cebola), o churrasco, a tartiflette, os pierogi e o salmorejo entre outros pratos, pero, a menos que queirades invitare a cear, centrémonos na razón pola que estades aquí, así que vos invito a botarlle un ollo a información máis relevante sobre min nas diferentes seccións desta páxina facendo click nos títulos de abaixo."
     },
     "france": {
         "en": "France",
@@ -394,6 +394,36 @@ const i18nKeyValues = {
         "es": "Contacto",
         "fr": "Contact",
         "gl": "Contacto"
+    },
+    "present": {
+        "en": "Present",
+        "es": "Presente",
+        "fr": "Présent",
+        "gl": "Presente"
+    },
+    "lisboa": {
+        "en": "Lisbon",
+        "es": "Lisboa",
+        "fr": "Lisbonne",
+        "gl": "Lisboa"
+    },
+    "portugal": {
+        "en": "Portugal",
+        "es": "Portugal",
+        "fr": "Portugal",
+        "gl": "Portugal"
+    },
+    "consultant-engineer": {
+        "en": "Consultant/Software Engineer",
+        "es": "Consultor/Ingeniero de software",
+        "fr": "Consultant/Ingénieur logiciel",
+        "gl": "Consultor/Enxeñeiro de software"
+    },
+    "altran-1": {
+        "en": "Software engineering consultacy for KLx (part of Crédit Agricole group).",
+        "es": "Consultoría de ingeniería de software para KLx (parte del grupo Crédit Agricole).",
+        "fr": "Consulting d'ingénierie logiciel pour KLx (groupe Crédit Agricole).",
+        "gl": "Consultoría de enxeñaría de software lara KLx (parte do grupo Crédit Agricole)."
     }
 }
 
